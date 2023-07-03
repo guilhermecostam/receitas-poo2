ab609377eb59c1ceea99eef33aad9fbde029ec2f
Guilherme Costa <guilhermecostadev@gmail.com>
2023-07-02 23:26:31 -0300

    feat: conclui a receita 7

--- 07/lib/main.dart
+++ 07/lib/main.dart
@@ -1,125 +1,434 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+class DataService {
+  final ValueNotifier<List<Map<String, dynamic>>> tableStateNotifier =
+      ValueNotifier([]);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  void loadData(int index, int? itemCount) async {
+    if (index == 0) {
+      await loadCoffees(itemCount);
+    } else if (index == 1) {
+      await loadBeers(itemCount);
+    } else if (index == 2) {
+      await loadNations(itemCount);
+    }
+  }
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  Future<void> loadCoffees(int? itemCount) async {
+    var coffeeUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/coffee/random_coffee',
+      queryParameters: {'size': '$itemCount'},
     );
+
+    var jsonString = await http.read(coffeeUri);
+    var coffeesJson = jsonDecode(jsonString);
+
+    tableStateNotifier.value = List.from(coffeesJson);
+  }
+
+  Future<void> loadBeers(int? itemCount) async {
+    var beersUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/beer/random_beer',
+      queryParameters: {'size': '$itemCount'},
+    );
+
+    var jsonString = await http.read(beersUri);
+    var beersJson = jsonDecode(jsonString);
+
+    tableStateNotifier.value = List.from(beersJson);
+  }
+
+  Future<void> loadNations(int? itemCount) async {
+    var nationsUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/nation/random_nation',
+      queryParameters: {'size': '$itemCount'},
+    );
+
+    var jsonString = await http.read(nationsUri);
+    var nationsJson = jsonDecode(jsonString);
+
+    tableStateNotifier.value = List.from(nationsJson);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+final dataService = DataService();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+void main() {
+  MyApp app = MyApp();
+  runApp(app);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  void _incrementCounter() {
+class _MyAppState extends State<MyApp> {
+  final DataService dataService = DataService();
+  int _selectedIndex = 0;
+  bool _showTable = false;
+  int? _itemCount = 5;
+
+  static const List<Map<String, dynamic>> _navBarItems = [
+    {
+      'label': 'Cafés',
+      'icon': Icon(Icons.coffee_outlined),
+      'columnNames': ['Nome', 'Origem', 'Variedade', 'Notas', 'Intensidade'],
+      'propertyNames': ['blend_name', 'origin', 'variety', 'notes', 'intensifier'],
+    },
+    {
+      'label': 'Cervejas',
+      'icon': Icon(Icons.local_drink_outlined),
+      'columnNames': ['Nome', 'Estilo', 'IBU'],
+      'propertyNames': ['name', 'style', 'ibu'],
+    },
+    {
+      'label': 'Nações',
+      'icon': Icon(Icons.flag_outlined),
+      'columnNames': ['Nacionalidade', 'Idioma', 'Capital', 'Esporte Nac'],
+      'propertyNames': ['nationality', 'language', 'capital', 'national_sport'],
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    dataService.loadData(_selectedIndex, _itemCount);
+  }
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
+      _showTable = true;
+      dataService.loadData(_selectedIndex, _itemCount);
+    });
+  }
+
+  void _onItemCountChanged(int? value) {
+    setState(() {
+      _itemCount = value;
+      dataService.loadData(_selectedIndex, _itemCount);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final navBarItem = _navBarItems[_selectedIndex];
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Dicas'),
+          actions: [
+            DropdownButton<int>(
+              value: _itemCount,
+              items: [
+                DropdownMenuItem<int>(
+                  value: 5,
+                  child: Text('5'),
+                ),
+                DropdownMenuItem<int>(
+                  value: 10,
+                  child: Text('10'),
+                ),
+                DropdownMenuItem<int>(
+                  value: 15,
+                  child: Text('15'),
+                ),
+              ],
+              onChanged: _onItemCountChanged,
             ),
           ],
         ),
+        body: Visibility(
+          visible: _showTable,
+          child: ValueListenableBuilder<List<Map<String, dynamic>>>(
+            valueListenable: dataService.tableStateNotifier,
+            builder: (context, value, child) {
+              return DataTableWidget(
+                jsonObjects: value,
+                columnNames: navBarItem['columnNames'],
+                propertyNames: navBarItem['propertyNames'],
+              );
+            },
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            for (var item in _navBarItems)
+              BottomNavigationBarItem(
+                label: item['label'],
+                icon: item['icon'],
+              ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class DataTableWidget extends StatelessWidget {
+  final List jsonObjects;
+  final List<String> columnNames;
+  final List<String> propertyNames;
+
+  DataTableWidget({this.jsonObjects = const [], this.columnNames = const [], this.propertyNames = const []});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: DataTable(
+        columns: columnNames
+            .map(
+              (name) => DataColumn(
+                label: Expanded(child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))),
+              ),
+            )
+            .toList(),
+        rows: jsonObjects
+            .map(
+              (obj) => DataRow(
+                cells: propertyNames
+                    .map(
+                      (propName) => DataCell(Text(obj[propName] ?? "")),
+                    )
+                    .toList(),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'dart:convert';
+
+class DataService {
+  final ValueNotifier<List<Map<String, dynamic>>> tableStateNotifier =
+      ValueNotifier([]);
+
+  void loadData(int index, int? itemCount) async {
+    if (index == 0) {
+      await loadCoffees(itemCount);
+    } else if (index == 1) {
+      await loadBeers(itemCount);
+    } else if (index == 2) {
+      await loadNations(itemCount);
+    }
+  }
+
+  Future<void> loadCoffees(int? itemCount) async {
+    var coffeeUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/coffee/random_coffee',
+      queryParameters: {'size': '$itemCount'},
+    );
+
+    var jsonString = await http.read(coffeeUri);
+    var coffeesJson = jsonDecode(jsonString);
+
+    tableStateNotifier.value = List.from(coffeesJson);
+  }
+
+  Future<void> loadBeers(int? itemCount) async {
+    var beersUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/beer/random_beer',
+      queryParameters: {'size': '$itemCount'},
+    );
+
+    var jsonString = await http.read(beersUri);
+    var beersJson = jsonDecode(jsonString);
+
+    tableStateNotifier.value = List.from(beersJson);
+  }
+
+  Future<void> loadNations(int? itemCount) async {
+    var nationsUri = Uri(
+      scheme: 'https',
+      host: 'random-data-api.com',
+      path: 'api/nation/random_nation',
+      queryParameters: {'size': '$itemCount'},
+    );
+
+    var jsonString = await http.read(nationsUri);
+    var nationsJson = jsonDecode(jsonString);
+
+    tableStateNotifier.value = List.from(nationsJson);
+  }
+}
+
+final dataService = DataService();
+
+void main() {
+  MyApp app = MyApp();
+  runApp(app);
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final DataService dataService = DataService();
+  int _selectedIndex = 0;
+  bool _showTable = false;
+  int? _itemCount = 5;
+
+  static const List<Map<String, dynamic>> _navBarItems = [
+    {
+      'label': 'Cafés',
+      'icon': Icon(Icons.coffee_outlined),
+      'columnNames': ['Nome', 'Origem', 'Variedade', 'Notas', 'Intensidade'],
+      'propertyNames': ['blend_name', 'origin', 'variety', 'notes', 'intensifier'],
+    },
+    {
+      'label': 'Cervejas',
+      'icon': Icon(Icons.local_drink_outlined),
+      'columnNames': ['Nome', 'Estilo', 'IBU'],
+      'propertyNames': ['name', 'style', 'ibu'],
+    },
+    {
+      'label': 'Nações',
+      'icon': Icon(Icons.flag_outlined),
+      'columnNames': ['Nacionalidade', 'Idioma', 'Capital', 'Esporte Nac'],
+      'propertyNames': ['nationality', 'language', 'capital', 'national_sport'],
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    dataService.loadData(_selectedIndex, _itemCount);
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _showTable = true;
+      dataService.loadData(_selectedIndex, _itemCount);
+    });
+  }
+
+  void _onItemCountChanged(int? value) {
+    setState(() {
+      _itemCount = value;
+      dataService.loadData(_selectedIndex, _itemCount);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final navBarItem = _navBarItems[_selectedIndex];
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Dicas'),
+          actions: [
+            DropdownButton<int>(
+              value: _itemCount,
+              items: [
+                DropdownMenuItem<int>(
+                  value: 5,
+                  child: Text('5'),
+                ),
+                DropdownMenuItem<int>(
+                  value: 10,
+                  child: Text('10'),
+                ),
+                DropdownMenuItem<int>(
+                  value: 15,
+                  child: Text('15'),
+                ),
+              ],
+              onChanged: _onItemCountChanged,
+            ),
+          ],
+        ),
+        body: Visibility(
+          visible: _showTable,
+          child: ValueListenableBuilder<List<Map<String, dynamic>>>(
+            valueListenable: dataService.tableStateNotifier,
+            builder: (context, value, child) {
+              return DataTableWidget(
+                jsonObjects: value,
+                columnNames: navBarItem['columnNames'],
+                propertyNames: navBarItem['propertyNames'],
+              );
+            },
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            for (var item in _navBarItems)
+              BottomNavigationBarItem(
+                label: item['label'],
+                icon: item['icon'],
+              ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
+
+class DataTableWidget extends StatelessWidget {
+  final List jsonObjects;
+  final List<String> columnNames;
+  final List<String> propertyNames;
+
+  DataTableWidget({this.jsonObjects = const [], this.columnNames = const [], this.propertyNames = const []});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: DataTable(
+        columns: columnNames
+            .map(
+              (name) => DataColumn(
+                label: Expanded(child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))),
+              ),
+            )
+            .toList(),
+        rows: jsonObjects
+            .map(
+              (obj) => DataRow(
+                cells: propertyNames
+                    .map(
+                      (propName) => DataCell(Text(obj[propName] ?? "")),
+                    )
+                    .toList(),
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
